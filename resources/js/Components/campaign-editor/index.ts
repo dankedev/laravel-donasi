f13943807e1c1fc1edc3bd7d01788c150f5054fc
Hadie Danker <hadie87@gmail.com>
2024-12-16 01:59:57 +0700

    update

--- resources/js/Components/campaign-editor/index.ts
+++
@@ -1,3 +0,0 @@
-export { CampaignSettingEditor } from "./campaign-setting";
-export { ContentCampaignEditor } from "./content-campaign-editor";
-export { CampaignFormProvider, useCampaignEditor } from "./editor-provider";
